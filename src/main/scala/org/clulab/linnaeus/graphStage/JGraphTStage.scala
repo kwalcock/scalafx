25dcade411382f5d5eefa2076afbae9537ec1b0d
Keith Alcock <github@keithalcock.com>
2019-03-29 14:23:07 -0700

    Separate graph building

--- src/main/scala/org/clulab/linnaeus/graphStage/JGraphTStage.scala
+++ src/main/scala/org/clulab/linnaeus/graphStage/JGraphTStage.scala
@@ -7,6 +7,7 @@ import org.clulab.linnaeus.StageManager
 
 import com.mxgraph.layout.mxCircleLayout
 import com.mxgraph.swing.mxGraphComponent
+
 import org.jgrapht.ListenableGraph
 import org.jgrapht.ext.JGraphXAdapter
 import org.jgrapht.graph.DefaultDirectedGraph
@@ -22,17 +23,8 @@ class JGraphTStage(stageManager: StageManager) extends GraphStage(stageManager) 
   val WIDTH = 400
   val HEIGHT = 400
 
-  title = "Linnaeus JGraphT Graph"
-  scene = new Scene(WIDTH, HEIGHT) {
-    val graph = new DefaultListenableGraph[String, DefaultEdge](new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]))
-    val jgxAdapter = new JGraphXAdapter[String, DefaultEdge](graph)
-    val component = new mxGraphComponent(jgxAdapter)
-    component.setConnectable(false)
-    component.getGraph.setAllowDanglingEdges(false)
 
-    val swingNode: SwingNode = new SwingNode()
-    swingNode.setContent(component)
-
+  protected def populateGraph(graph: DefaultListenableGraph[String, DefaultEdge]): Unit = {
     val v1 = "v1"
     val v2 = "v2"
     val v3 = "v3"
@@ -47,17 +39,32 @@ class JGraphTStage(stageManager: StageManager) extends GraphStage(stageManager) 
     graph.addEdge(v2, v3)
     graph.addEdge(v3, v1)
     graph.addEdge(v4, v3)
+  }
 
-    // positioning via jgraphx layouts
+  protected def mkLayout(jgxAdapter: JGraphXAdapter[String, DefaultEdge]): mxCircleLayout = {
     val layout = new mxCircleLayout(jgxAdapter)
-
-    // center the circle
     val radius = 100
+
     layout.setX0((WIDTH / 2.0) - radius)
     layout.setY0((HEIGHT / 2.0) - radius)
     layout.setRadius(radius)
     layout.setMoveCircle(true)
+    layout
+  }
 
+  title = "Linnaeus JGraphT Graph"
+  scene = new Scene(WIDTH, HEIGHT) {
+    val graph = new DefaultListenableGraph[String, DefaultEdge](new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]))
+    populateGraph(graph)
+
+    val jgxAdapter = new JGraphXAdapter[String, DefaultEdge](graph)
+    val swingNode: SwingNode = new SwingNode()
+    val component = new mxGraphComponent(jgxAdapter)
+    component.setConnectable(false)
+    component.getGraph.setAllowDanglingEdges(false)
+    swingNode.setContent(component)
+
+    val layout = mkLayout(jgxAdapter)
     layout.execute(jgxAdapter.getDefaultParent)
 
     val borderPane = new BorderPane() {
