792c9806fb4f3447ab822e0ba8924ce9a0458e27
Keith Alcock <github@keithalcock.com>
2019-04-22 23:13:23 -0700

    Fix GraphNode

--- src/main/scala/org/clulab/linnaeus/model/GraphNode.scala
+++ src/main/scala/org/clulab/linnaeus/model/GraphNode.scala
@@ -1,7 +1,4 @@
 package org.clulab.linnaeus.model
 
-import org.clulab.linnaeus.model.Identifyable
-import org.clulab.linnaeus.model.Valuable
-
-class GraphNode {
+class GraphNode[T](val data: T) {
 }
