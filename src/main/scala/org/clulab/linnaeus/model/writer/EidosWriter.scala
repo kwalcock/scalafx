af3d34ef5ad7ffefa4102589533f6fd84a9ccee6
Keith Alcock <github@keithalcock.com>
2019-05-05 09:33:10 -0700

    Add Tabber

--- src/main/scala/org/clulab/linnaeus/model/writer/EidosWriter.scala
+++ src/main/scala/org/clulab/linnaeus/model/writer/EidosWriter.scala
@@ -4,22 +4,19 @@ import org.clulab.linnaeus.model.graph.eidos.EidosNetwork
 import org.clulab.linnaeus.model.graph.robert.RobertNetwork
 import org.clulab.linnaeus.util.Closer.AutoCloser
 import org.clulab.linnaeus.util.FileUtil
+import org.clulab.linnaeus.util.Tabber
 
 class EidosWriter(val baseFilename: String) extends Writer {
 
   def write(network: EidosNetwork): Unit = {
     FileUtil.newPrintWriter(baseFilename + EidosWriter.FILE_END).autoClose { printWriter =>
-
-      def tab(depth: Int): Unit = {
-        0.until(depth * 2).foreach(_ => printWriter.print(' '))
-      }
+      val tabber = new Tabber(2)
 
       def dumpList(depth: Int, name: String, values: Seq[String]): Unit = {
         if (values.nonEmpty) {
-          tab(depth); printWriter.println(s"$name:")
-
+          printWriter.println(tabber.tab(depth, s"$name:"))
           values.foreach { value =>
-            tab(depth + 1); printWriter.println(s"- $value")
+            printWriter.println(tabber.tab(depth + 1, s"- $value"))
           }
         }
       }
@@ -29,17 +26,17 @@ class EidosWriter(val baseFilename: String) extends Writer {
       // However, descriptions won't match because they span multiple lines.
       def dumpRecord(nodeRecord: network.NodeRecord, depth: Int): Unit = {
         if (nodeRecord.isLeaf) {
-          tab(depth); printWriter.println("- OntologyNode:")
+          printWriter.println(tabber.tab(depth, "- OntologyNode:"))
           dumpList(depth + 1, "pattern", nodeRecord.node.patterns)
           dumpList(depth + 1, "examples", nodeRecord.node.examples)
           dumpList(depth + 1, "descriptions", nodeRecord.node.descriptions)
-          tab(depth + 1); printWriter.println(s"name: ${nodeRecord.node.name}")
+          printWriter.println(tabber.tab(depth + 1, s"name: ${nodeRecord.node.name}"))
           nodeRecord.node.polarityOpt.foreach { polarity =>
-            tab(depth + 1); printWriter.println(s"polarity: $polarity")
+            printWriter.println(tabber.tab(depth + 1, s"polarity: $polarity"))
           }
         }
         else {
-          tab(depth); printWriter.println(s"- ${nodeRecord.node.name}:")
+          printWriter.println(tabber.tab(depth, s"- ${nodeRecord.node.name}:"))
           nodeRecord.outgoing.foreach { edgeRecord =>
             dumpRecord(edgeRecord.targetRecord, depth + 1)
           }
